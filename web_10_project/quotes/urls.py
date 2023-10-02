2814ef3d09396b22ed669e6f35cde8f129b1a65a
paukdv <paukdv@gmail.com>
2023-10-02 19:29:07 +0300

    Initial commit

---
+++ web_10_project/quotes/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+app_name = "quotes"
+urlpatterns = [
+    path("", views.main, name="root"),
+    path("<int:page>", views.main, name="root_paginate"),
+    path('add_author/', views.add_author, name='add_author'),
+    path('add_quote/', views.add_quote, name='add_quote'),
+    path('author/<int:author_id>', views.author_about, name='author'),
+]
